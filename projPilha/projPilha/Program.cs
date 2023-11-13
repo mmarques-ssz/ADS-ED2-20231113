7cc3f11241b7922f971c293fe5d13b9a4affb4b0
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-11-13 20:19:09 -0300

    Add files via upload

---
+++ projPilha/projPilha/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> pilhaInt;
+            pilhaInt = new Stack<int>();
+
+            pilhaInt.Push(10);
+            pilhaInt.Push(20);
+            pilhaInt.Push(30);
+            pilhaInt.Push(40);
+            pilhaInt.Push(50);
+
+            mostraPilha(pilhaInt);
+
+            Console.WriteLine("Topo da pilha: {0}", pilhaInt.Peek());
+            Console.WriteLine("Primeiro elemento da pilha: {0}", pilhaInt.First());
+            Console.WriteLine("Último elemento da pilha: {0}", pilhaInt.Last());
+            mostraPilha(pilhaInt);
+
+            Console.WriteLine("Saiu da pilha: {0}", pilhaInt.Pop());
+            mostraPilha(pilhaInt);
+
+            Console.WriteLine("Saiu da pilha: {0}", pilhaInt.Pop());
+            Console.WriteLine("Saiu da pilha: {0}", pilhaInt.Pop());
+            mostraPilha(pilhaInt);
+
+        }
+
+        static void mostraPilha(Stack<int> pilha)
+        {
+            int k;
+            Console.WriteLine("-- Qtde de elementos: {0} --", pilha.Count);
+            k = 0;
+            foreach(int i in pilha)
+            {
+                Console.WriteLine("{0}º elemento: {1}", ++k, i);
+            }
+            Console.WriteLine("----------------------");
+        }
+
+
+    }
+}
